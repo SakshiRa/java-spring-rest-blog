8be1aa9ee928ec71edb4284e038426ea5accf5f1
Sakshi Rathi <64864094+SakshiRa@users.noreply.github.com>
2020-06-07 18:40:55 -0700

    last one

--- src/main/java/com/pluralsight/blog/data/AuthorRepository.java
+++ src/main/java/com/pluralsight/blog/data/AuthorRepository.java
@@ -2,9 +2,12 @@ package com.pluralsight.blog.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.pluralsight.blog.model.Author;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import javax.persistence.ManyToOne;
 
+
+@RepositoryRestResource(exported = false)
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
 }
