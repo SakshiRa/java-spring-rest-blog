ee31b65970bab67252c0c0f6bf20af60d01317e6
Sakshi Rathi <64864094+SakshiRa@users.noreply.github.com>
2020-06-07 18:30:50 -0700

    Update DatabaseLoader.java

--- src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
+++ src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
@@ -39,11 +39,11 @@ public class DatabaseLoader implements ApplicationRunner {
         ));
 
         authorRepository.saveAll(authors);
-        
+
         IntStream.range(0,40).forEach(i->{
             String template = templates[i % templates.length];
             String gadget = gadgets[i % gadgets.length];
-            Author author = authors.get(i%authors.size());
+            Author author = authors.get(i % authors.size());
 
             String title = String.format(template, gadget);
             Post post = new Post(title, "Lorem ipsum dolor sit amet, consectetur adipiscing elit… ");
