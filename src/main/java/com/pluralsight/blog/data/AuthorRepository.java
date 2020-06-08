25d621bb174e7150b427d758e5a4536dac04b5f2
Sakshi Rathi <64864094+SakshiRa@users.noreply.github.com>
2020-06-07 17:09:16 -0700

    author'

--- src/main/java/com/pluralsight/blog/data/AuthorRepository.java
+++ src/main/java/com/pluralsight/blog/data/AuthorRepository.java
@@ -1,21 +1,11 @@
 package com.pluralsight.blog.data;
 
-import com.pluralsight.blog.model.Author;
-import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.stereotype.Component;
+import com.pluralsight.blog.model.Author;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.persistence.ManyToOne;
 
-@Component
-public class AuthorRepository {
-    public List<Author> findAll() {
-        return new ArrayList<>();
-    }
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+    @ManyToOne(fetch = FetchType.EAGER)
 
-    public List<Author> saveAll(List<Author> authors) {
-        return new ArrayList<>();
-    }
 }
